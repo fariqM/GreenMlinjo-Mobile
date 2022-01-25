adc868e4783efb4cf0e96f841c6ac029b241260e
fariqM <maulana.fariq30@gmail.com>
2022-01-25 22:53:40 +0700

    update configuration for costume advance theming

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+      options: {
+        customProperties: true,
+      },
+    themes: {
+      light: {
+        primary: "#87BD43",
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: "#FF0000",
+        info: "#0EC1EF",
+        success: "#09893C",
+        warning: '#FFC107',
+        black: "#000000",
+      },
+    },
+  },
+});
