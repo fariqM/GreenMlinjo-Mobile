83e55fa00f4169116e04bffdbaa795219d3a6744
fariqM <maulana.fariq30@gmail.com>
2022-01-26 12:26:52 +0700

    create landing apps

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/guest/Login.vue'
+import Register from '../views/guest/Register.vue'
+import Landing from '../views/guest/Landing.vue'
+
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'landing',
+    component: Landing
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'login',
+    component: Landing
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Landing
+  },
+  
 ]
 
 const router = new VueRouter({
---
+++ src/views/guest/Landing.vue
@@ -0,0 +1,81 @@
+<template>
+	<div>
+		<!-- <v-btn fab>asdasdasd</v-btn> -->
+		<v-carousel
+			cycle
+			v-model="model"
+			hide-delimiter-background
+			height="85vh"
+			hide-delimiters
+			:show-arrows="false"
+			delimiter-icon="mdi-minus"
+		>
+			<v-carousel-item v-for="(item, i) in images" :key="i">
+				<v-sheet height="100%">
+					<v-row class="fill-height" align="center" justify="center">
+						<v-img max-height="270" max-width="250" :src="item"> </v-img>
+						<!-- '../../assets/images/cart.png' -->
+					</v-row>
+				</v-sheet>
+			</v-carousel-item>
+		</v-carousel>
+		<div class="mb-4 d-flex justify-center">
+			<v-btn color="primary">gabung sekarang</v-btn>
+		</div>
+		<v-row style="margin: 0px; display: block" no-gutters justify="center">
+			<div class="d-flex justify-space-around">
+				<div class="" style="max-width: 140px; min-width: 140px">
+					<v-btn text color="secondary"><b>Lewati</b></v-btn>
+				</div>
+
+				<div class="d-flex align-center">
+					<span
+						v-for="(item, i) in images"
+						:key="i"
+						class="costum_corrousel_controls"
+						:class="[i === model ? 'ccc_active' : '']"
+					/>
+				</div>
+				<div class="" style="max-width: 140px; min-width: 140px">
+					<v-btn color="primary pr-2" text @click="model++">Selanjutnya</v-btn>
+				</div>
+			</div>
+		</v-row>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			model: 0,
+			tes: "../../assets/images/box.png",
+			images: [
+				"assets/images/box.png",
+				"assets/images/small_box.png",
+				"assets/images/cart.png",
+			],
+		};
+	},
+};
+</script>
+
+<style lang="scss">
+.v-carousel__controls__item {
+	margin: 0px;
+}
+</style>
+
+<style scoped>
+.costum_corrousel_controls {
+	margin: 0px 5px;
+	height: 9px;
+	width: 9px;
+	background-color: #c5ff7cd9;
+	border-radius: 50%;
+	display: inline-block;
+}
+.ccc_active {
+	background-color: #87bd43;
+}
+</style>
